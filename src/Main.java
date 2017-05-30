9a5ade144f528b97ea7a1f9d25ff534d3e6bd323
yianglee <ean122@gmail.com>
2017-05-29 18:13:34 -0700

    fix the indentation

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,13 @@
  * UseCase2 to print the entire contact list.
  * UseCase3 to retrieve a person's information by last name.
  * UseCase4 to quit the program and save to disk.
- * UseCase5 to start the program and open existing file on disk. 
+ * UseCase5 to start the program and open existing file on disk.
  */
 
 public class Main {
-    
+
     public static void main(String[] args) {
-    GuiApp guiApp;
-    guiApp = new GuiApp();
+        GuiApp guiApp;
+        guiApp = new GuiApp();
     }
 }
