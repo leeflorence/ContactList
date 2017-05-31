8639aeb8733cf21f4869858bcb6af0864aa1362f
Florence Lee <leeflorence@users.noreply.github.com>
2017-05-31 13:48:44 -0700

    Update Contact.java

--- src/Contact.java
+++ src/Contact.java
@@ -5,32 +5,3 @@ import java.io.*;
  * street address, email address, phone number and notes of an individual. 
  */
 
-public class Contact {
-    private String first;
-    private String last;
-    private String streetAddress;
-    private String email;
-    private String phone; 
-    private String notes;
-    
-    
-    /**
-     * Constructor for Contact. 
-     */
-    public Contact(String first, String last, String streetAddress, 
-                   String email, String phone, String notes) {
-        this.first = first;
-        this.last = last;
-        this.streetAddress = streetAddress;
-        this.email = email;
-        this.phone = phone;
-        this.notes = notes;
-    }
-  
-    /**
-     * Returns a String that contains the contact information. 
-     */
-    public String toString() {
-        return "";
-    }
-}
